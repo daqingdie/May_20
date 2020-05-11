db29dc1a09429b79358397ac751f92f80f8dc360
WuLieQing <532131417@qq.com>
2020-05-11 11:58:46 +0800

    添加了测试axios的get和post的前后端交互及jpa事务管理的代码

--- src/main/java/com/example/webpack/controller/JPAController.java
+++ src/main/java/com/example/webpack/controller/JPAController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,6 +16,9 @@ import java.util.logging.Logger;
 public class JPAController {
     @Autowired
     private UserService userService;
+
+    //获取日志记录器Logger，名字为本类类名
+    private static Logger log = Logger.getLogger("com.lq.JPAController");
 
 
     @GetMapping("/findByPage")
@@ -35,6 +39,31 @@ public class JPAController {
         Logger.getLogger("myerr").log(Level.INFO,String.valueOf(id));
         userService.deleteUser(id);
         return "ok";
+    }
+
+    @GetMapping("/axiosGetNews")
+    public String axiosGetNews(String message){
+        System.out.println(message);
+        return "服务器发送的消息";
+    }
+
+    @PostMapping("/axiosPostNews")
+    public String axiosPostNews(@RequestBody String message){
+        System.out.println(message);
+        return "服务器发送的消息";
+    }
+
+    @GetMapping("/testTrans")
+    public String testTrans(Integer page, Integer size) {
+        log.info(page.toString());
+        try {
+            userService.testTrans();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            return e.toString();
+        }
+        return "点击成功";
     }
 
 
