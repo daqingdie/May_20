cef8dbb3136320e791ac924736c0da6e095cd53a
WuLieQing <532131417@qq.com>
2020-05-11 11:56:42 +0800

    添加了有关日志学习和jdk8新特性stream流的测试代码

--- src/test/java/com/example/webpack/JPATest.java
+++ src/test/java/com/example/webpack/JPATest.java
@@ -9,8 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,18 +22,37 @@ public class JPATest {
     @Autowired
     private UserRepository userRepository;
 
+    //获取日志记录器Logger，名字为本类类名
+    private static Logger log = Logger.getLogger("com.lq.JpaTest");
+
     @Test
     public void myTest() {
         System.out.println("哈哈");
-//        List<User> userList = userRepository.findAll();
+        List<User> users = userRepository.findAll();
 //        for (User user : userList) {
 //            System.out.println(user.getUsername());
 //        }
-        User user = userRepository.findByUsername("求职者测试员").get(0);
-        System.out.println(user.getUsername()+user.getPhone());
-        List<User> users= userRepository.findByUsernameLike("%求职者%");
-        for (User user1 : users) {
-            System.out.println(user1.toString());
+
+//        User user = userRepository.findByUsername("求职者测试员").get(0);
+//        System.out.println(user.getUsername()+user.getPhone());
+//        List<User> users= userRepository.findByUsernameLike("%求职者%");
+//        for (User user1 : users) {
+//            log.log(Level.INFO,user.toString());
+//        }
+        List<User> collect = users.stream().filter(s->{
+            if(s.getId()>10&&s.getId()<15)
+                return true;
+            return false;
+        }).map(u->{
+            u.setUsername("哈哈哈哈");
+            return u;
+        }).sorted().collect(Collectors.toList());
+
+        for (User user1 : collect) {
+            log.info(user1.toString());
         }
+
+
+
     }
 }
---
+++ src/test/java/com/example/webpack/Mytest.java
@@ -0,0 +1,6 @@
+package com.example.webpack;
+
+import com.alibaba.fastjson.JSON;
+
+public class Mytest {
+}
