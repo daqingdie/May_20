cc735daf2ccb49cd685000634ee64fed5059b46b
WuLieQing <532131417@qq.com>
2020-05-13 16:40:52 +0800

    将返回的数据类型由User变为Juser

--- src/main/java/com/example/webpack/controller/JsonController.java
+++ src/main/java/com/example/webpack/controller/JsonController.java
@@ -1,12 +1,15 @@
 package com.example.webpack.controller;
 
 
+import com.example.webpack.POJO.JUser;
 import com.example.webpack.POJO.User;
 import com.example.webpack.Util.AjaxResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @Slf4j
 @RestController
@@ -17,7 +20,7 @@ public class JsonController {
     @PostMapping("/testJson")
     public AjaxResponse testJson(@RequestBody User user) {
         log.info(user.toString());
-        User user1 = new User(0, "测试用户", "123", "10086", 2);
+        JUser user1 = new JUser(0L, "测试用户",new Date());
         return AjaxResponse.success(user1);
     }
 
