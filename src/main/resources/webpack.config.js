0bc5a20f3fe4616d38aae8afe49616bbc30d59b2
WuLieQing <532131417@qq.com>
2020-05-14 09:56:20 +0800

    用webpack打包js文件

--- src/main/resources/webpack.config.js
+++ src/main/resources/webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path');
+const path = require('path');  //node.js中的函数
 
 module.exports={
     entry: './static/js/cart.js',
     output:{
-        path:path.resolve(__dirname,'static/js'),
+        path:path.resolve(__dirname,'static/js'), //获取当前的绝对路径
         filename:'myMain.js',
 
     }
